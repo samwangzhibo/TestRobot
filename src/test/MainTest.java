64821ec956f8a1e9d6089a46ac3b06d169e64490
王智博 <245353898@qq.com>
2015-08-13 17:52:39 +0800

    beta2版本

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -8,23 +8,29 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.InputMismatchException;
+import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.Scanner;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 public class MainTest {
 	public static void main(String[] args) throws Exception{
 		Scanner scan = new Scanner(System.in);
-		
+		scan.useDelimiter("\n");//修改输入数据的分隔符
 		String INFO;
 		System.out.println("主人请输入你的指令：");
 		
 		while(scan.hasNext()){
 			String info = scan.nextLine();
-			System.out.println("输入info为："+info);
-				INFO = URLEncoder.encode(info, "utf-8");
+			//System.out.println("输入info为："+info);
+				//INFO = URLEncoder.encode(info, "utf-8");
 				
-				//byte[] b = info.getBytes("utf-8");  
-		        //INFO =new String(b,"utf-8");
-				
+				byte[] b = info.getBytes("utf-8");
+		        
+		        INFO =new String(b,"utf-8");
 				String getUrl = util.API_ENTRY+"?key="+util.APIKEY+"&info="+INFO;
 				URL url;
 				url = new URL(getUrl);
@@ -42,10 +48,38 @@ public class MainTest {
 				
 				br.close();
 				urlcon.disconnect();
-				System.out.println(util.getJsonText(sb));
+				//String Type_ID = util.getType(new String(sb));
+				JSONObject jobj = util.getJsonObjFromSB(sb);
+				if(util.getType(new String(sb))==null){
+					System.out.println("不识别类型。。。。");
+				}else{
+				System.out.println("类型：	"+util.getType(new String(sb)));
+				System.out.println("助手：	"+util.getJsonText(sb));
+				if(util.getJsonObjFromSB(sb).get("url")!=null){
+				System.out.println("URL：	"+util.getJsonObjFromSB(sb).get("url"));
+				}
+				if(util.getJsonObjFromSB(sb).get("list")!=null){
+					//舒展业务
+					JSONArray ja =  util.getJsonObjFromSB(sb).getJSONArray("list");
+					//System.out.println(ja);
+					JSONObject job =  ja.getJSONObject(0);
+					Iterator it = job.keys();
+					while(it.hasNext()){
+						String key = (String) it.next();
+						String value = (String) job.get(key);
+						System.out.print(key+" : "+value+" |||  ");
+					}
+					System.out.println();
+					
+				}
+				}
 				System.out.println("主人是否继续？继续 1  不继续-1");
-				int a = scan.nextInt();
-				if(a==1){
+				System.out.println("----------------精美的换行符------------");
+				
+				String a= scan.nextLine();
+				//System.out.println("输入指令为"+a);
+				
+				if(a.equals("1")){
 					System.out.println("主人请输入你的指令：");
 				}else {
 					System.out.println("退出喽。。。");
--- src/test/util.java
+++ src/test/util.java
@@ -5,6 +5,12 @@ import net.sf.json.JSONObject;
 public class util {
 	public static String APIKEY = "54d263a2970dcda61ef214c4852a8dc3";
 	public static String API_ENTRY = "http://www.tuling123.com/openapi/api";
+	public static String TYPE_Text = "文本";
+	public static String TYPE_Link = "链接";
+	public static String TYPE_TRAIN = "列车";
+	public static String TYPE_NEWS = "新闻";
+	public static String TYPE_Flight = "航班";
+	public static String TYPE_Cai = "菜";
 	public static String getJsonText(StringBuffer sb){
 		String temp = new String(sb);
 		JSONObject jsonObject = JSONObject.fromObject(temp);
@@ -12,5 +18,32 @@ public class util {
 			return (String)jsonObject.get("text");
 		}
 		else return null;
+	}
+	
+	public static String getType(String sbb){
+		int sb=(Integer) JSONObject.fromObject(sbb).get("code");
+		if(sb==100000){
+			return TYPE_Text;
+		}else if(sb==200000){
+			return TYPE_Link;
+		}else if(sb==302000){
+			return TYPE_NEWS;
+		}else if(sb==305000){
+			return TYPE_TRAIN;
+		}else if(sb==306000){
+			return TYPE_Flight;
+		}else if(sb==308000){
+			return TYPE_Cai;
+		}else return null;
+	}
+	/**
+	 * 
+	 * @param sb
+	 * @return
+	 */
+	public static JSONObject getJsonObjFromSB(StringBuffer sb){
+		String temp = new String(sb);
+		JSONObject jsonObject = JSONObject.fromObject(temp);
+		return jsonObject;
 	}
 }
